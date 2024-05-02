b466619a2602618663d392260fa4363bc6f77a04
Gaurav Chauhan <gaurav.2.gaurav@nokia.com>
2024-05-02 22:17:12 +0530

    girst commfi

---
+++ project1_madlibs.py
@@ -0,0 +1,7 @@
+# we have a paragraph with blanks , we have to fill those blanks 
+name = input("please enter your name  ")
+company = input("please enter the company you work ")
+country = input("please enter your country  ")
+age = int(input("please enter your age  "))
+str = f"hey my name is {name} , I am {age} years old ,I am from {country} I works in {company}   "
+print(str)
---
+++ project2_guess-the-number-computer.py
@@ -0,0 +1,13 @@
+# Computer has a secret number and we are guessing that 
+import random
+computer = random.randint(1,10)
+user=0
+while(user != computer):
+    user = int(input("please enter the number between 1 to 10  "))
+    if user < computer:
+        print("user is low")
+    elif user >computer:
+        print("user is high")
+    else:
+        print("both are equal congratualtions")
+    
