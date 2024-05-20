7c361ecc3a65f086d5ba787db8f6ab216d8683af
Gaurav Chauhan <gaurav.2.gaurav@nokia.com>
2024-05-21 00:36:35 +0530

    regression calci

--- project10-regression_calulator.py
+++ project10-regression_calulator.py
@@ -10,24 +10,31 @@ def multiplication(a,b):
 def division(a,b):
     return a/b
 
-operation = {"+":add, "-":substraction, "*":multiplication , "/":division}
-num1 = int(input("please enter first number \t "))
+def calculator():
+    operation = {"+":add, "-":substraction, "*":multiplication , "/":division}
+    num1 = int(input("please enter first number \t "))
 
-for keys in operation:
-    print(keys)
-    
-continue_operation = True
-while continue_operation != False:   
-    output_symbol = input("Please select the operation \t")
-    num2 = int(input("please enter second number \t "))
-    caluator_function = operation[output_symbol]
-    output = caluator_function(num1,num2)
-    print(output)
-
-    next_operation = input(f"enter y to continue with {output} , n to exit \t").lower()
-    if next_operation == 'y':
+    for keys in operation:
+        print(keys)
+        
+    continue_operation = True
+    while continue_operation != False:   
+        output_symbol = input("Please select the operation \t")
         num2 = int(input("please enter second number \t "))
-    
+        caluator_function = operation[output_symbol]
+        output = caluator_function(num1,num2)
+        print(output)
+
+        next_operation = input(f"enter y to continue with {output} ,n to continue new calculation,q to quit \t").lower()
+        if next_operation == 'y':
+            num1 = output
+        elif next_operation == 'n':
+            continue_operation = False
+            calculator()
+        else:
+            continue_operation=False
+            print("claculation over")
+calculator()
     
 
 
